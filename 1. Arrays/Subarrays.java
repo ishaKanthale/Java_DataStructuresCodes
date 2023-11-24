f63530587b5eefa3e7071cc417a307549a824c39
Isha Kanthale <110197912+ishaKanthale@users.noreply.github.com>
2023-11-23 21:41:03 -0800

    Create Subarrays.java

---
+++ 1. Arrays/Subarrays.java
@@ -0,0 +1,25 @@
+package Arrays;
+
+public class Subarrays {
+
+    //  Print Subarrays
+    public static void subarrays(int arr[]) {
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                for (int k = i; k <= j; k++) {
+                    System.out.print(arr[k] + " ");
+                }
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int numbers[] = {2, 4, 6, 8, 10};
+
+        subarrays(numbers);
+    }
+}
